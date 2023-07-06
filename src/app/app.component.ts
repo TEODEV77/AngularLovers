51733d929c38581fada6216cde6acd51d8022baf
Mateo <mateo.orolot@gmail.com>
2023-07-05 22:12:20 -0500

    New Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular_lovers';
+  public userInfo: userInfo[] = [];
+
+  public buttonState: boolean = false;
+
+  public user: userInfo = {
+    nickname: '',
+    comment: '',
+  };
+
+  constructor() {}
+
+  onSave() {
+    this.userInfo.push(this.user);
+    this.user = {
+      nickname: '',
+      comment: '',
+    };  
+  }
+}
+
+interface userInfo {
+  nickname: string;
+  comment: string;
 }
